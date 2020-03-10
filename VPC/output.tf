cadebad654f5335855d83754300c0cb0efe60766
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-10 23:56:58 +0000

    add

---
+++ VPC/output.tf
@@ -0,0 +1,13 @@
+output "VPC_ID" {
+  value       = "${aws.vpc.dev.id}"
+ 
+}
+output "VPC_ID" {
+  value       = "${aws.vpc.dev.id}"
+ 
+}
+
+output "Subnet1" {
+  value       = "${aws.vpc.dev1.id}"
+ 
+}
