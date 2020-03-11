4dd91e7159d45e82267b9f5fcef08f2dbd3d24a5
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-11 00:32:39 +0000

    1

--- VPC/backand.tf
+++ VPC/backand.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-class-andrii"
-    key    = "path/to/my/key"
+    key    = "path/to/my/vpc"
     region = "us-east-1"
     #dynamodb_table = "terraform-class"
   }
--- VPC/output.tf
+++ VPC/output.tf
@@ -1,13 +1,32 @@
+  
 output "VPC_ID" {
-  value       = "${aws.vpc.dev.id}"
- 
-}
-output "VPC_ID" {
-  value       = "${aws.vpc.dev.id}"
- 
+  value       = "${aws_vpc.dev.id}"
 }
 
 output "Subnet1" {
-  value       = "${aws.vpc.dev1.id}"
- 
-}
+  value       = "${aws_subnet.dev1.id}"
+  }
+
+output "Subnet2" {
+  value       = "${aws_subnet.dev2.id}"
+  }
+
+output "Subnet3" {
+  value       = "${aws_subnet.dev3.id}"
+  }
+
+output "Private Subnet1" {
+  value       = "${aws_subnet.dev_private1.id}"
+  }
+
+output "Private Subnet2" {
+  value       = "${aws_subnet.dev_private2.id}"
+  }
+
+output "Private Subnet3" {
+  value       = "${aws_subnet.dev_private3.id}"
+  }
+
+output "IGW" {
+    value = "${aws_internet_gateway.dev.id}"
+  }
