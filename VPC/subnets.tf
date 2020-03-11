07b2b5bf23fb375f3a935ce929b813a933cadee7
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-11 00:42:40 +0000

    /

--- VPC/subnets.tf
+++ VPC/subnets.tf
@@ -7,28 +7,28 @@ resource "aws_subnet" "dev1" {
 
 resource "aws_subnet" "dev2" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = $"{var.cidr_block2_public}" 
+  cidr_block = "${var.cidr_block2_public}" 
 }
 
 resource "aws_subnet" "dev3" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = $"{var.cidr_block3_public}" 
+  cidr_block = "${var.cidr_block3_public}" 
 }
 
 
 # Private Subnets
 resource "aws_subnet" "dev_private1" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = ${var.cidr_block1_private}" 
+  cidr_block = "${var.cidr_block1_private}" 
   } 
 
 
 resource "aws_subnet" "dev_private2" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = ${var.cidr_block2_private}" 
+  cidr_block = "${var.cidr_block2_private}" 
 }
 
 resource "aws_subnet" "dev_private3" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = ${var.cidr_block3_private}" 
+  cidr_block = "${var.cidr_block3_private}" 
 }
--- VPC/variable.tf
+++ VPC/variable.tf
@@ -1,11 +1,11 @@
 variable "region" {} 
 
-variable "cider_block" {}
+variable "cidr_block" {}
 
-variable "cider_block1_public" {}
-variable "cider_block2_public" {}
-variable "cider_block3_public" {}
+variable "cidr_block1_public" {}
+variable "cidr_block2_public" {}
+variable "cidr_block3_public" {}
 
-variable "cider_block1_private" {}
-variable "cider_block2_private" {}
-variable "cider_block3_private" {}
+variable "cidr_block1_private" {}
+variable "cidr_block2_private" {}
+variable "cidr_block3_private" {}
