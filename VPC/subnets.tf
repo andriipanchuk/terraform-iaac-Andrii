4f06334123cea6008379ecfe4160fb86cdaf919c
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-11 00:37:18 +0000

    1

--- VPC/subnets.tf
+++ VPC/subnets.tf
@@ -7,12 +7,12 @@ resource "aws_subnet" "dev1" {
 
 resource "aws_subnet" "dev2" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = ${var.cidr_block2_public}" 
+  cidr_block = $"{var.cidr_block2_public}" 
 }
 
 resource "aws_subnet" "dev3" { 
   vpc_id = "${aws_vpc.dev.id}" 
-  cidr_block = ${var.cidr_block3_public}" 
+  cidr_block = $"{var.cidr_block3_public}" 
 }
 
 
---
+++ VPC/variable.tf
@@ -0,0 +1,11 @@
+variable "region" {} 
+
+variable "cider_block" {}
+
+variable "cider_block1_public" {}
+variable "cider_block2_public" {}
+variable "cider_block3_public" {}
+
+variable "cider_block1_private" {}
+variable "cider_block2_private" {}
+variable "cider_block3_private" {}
