f5c0a5c32ffc9ecb8bba2342f5dcbafaafde4bcb
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-17 20:59:50 +0000

    1

--- VPC/configurations/us-east-1/vpc.tfvars
+++ VPC/configurations/us-east-1/vpc.tfvars
@@ -1,9 +1,11 @@
 # Provide a region for VPC
 environment                     =   	"virginia"
-
-
-
 region = "us-east-1"
+
+# Choose Azs to create subnets
+az1             = "us-east-1a"
+az2             = "us-east-1b"
+az3             = "us-east-1c"
 
 s3_bucket                       =	"terraform-class-andrii"
 s3_folder_region                =	"us-east-1"
--- VPC/module/variable.tf
+++ VPC/module/variable.tf
@@ -1,4 +1,4 @@
-variable "region" {} 
+variable "region" {}
 
 variable "cidr_block" {}
 
@@ -6,6 +6,13 @@ variable "cidr_block1_public" {}
 variable "cidr_block2_public" {}
 variable "cidr_block3_public" {}
 
+
 variable "cidr_block1_private" {}
 variable "cidr_block2_private" {}
 variable "cidr_block3_private" {}
+
+
+
+variable "az1"      {}
+variable "az2"      {}
+variable "az3"      {}
