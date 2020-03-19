5b539811efda0e99461a1ca8837d6bcc298ed347
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-19 19:37:04 +0000

    .

--- 3-tier-app/app.tf
+++ 3-tier-app/app.tf
@@ -1,25 +1,33 @@
-data "terraform_remote_state" "dev" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-class-andrii"
-    key    = "tower/us-east-1/tools/virginia/tower.tfstate"
-    region = "us-east-1"
-  }
-}
-
-# Gets ubuntu  info
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+# Creates ASG with Lauch Config
+module "wordpress" {
+  name   = "wordpress"
+  source = "terraform-aws-modules/autoscaling/aws"
+  version = "2.12.0"
+  lc_name = "wordpress-lc"
+  image_id        = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow_tls.id}"]
+  asg_name                  = "wordpress-asg"
+  vpc_zone_identifier       = [
+                                "${data.terraform_remote_state.dev.Subnet1}",
+                                "${data.terraform_remote_state.dev.Subnet2}",
+                                "${data.terraform_remote_state.dev.Subnet3}",
+                                ]
+  health_check_type         = "EC2"
+  min_size                  = 3
+  max_size                  = 48
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+tags = [
+    {
+      key                 = "Environment"
+      value               = "dev"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+  ]
 }
