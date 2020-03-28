5a4995fc8e28d969bc4b74d3ffe3feb076215dd9
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-28 05:09:18 +0000

    update

--- aws/VPC/module/vpc.tf
+++ aws/VPC/module/vpc.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "dev" { 
-
-  cidr_block = "${var.cidr_block}" 
+ cidr_block = "${var.cidr_block}"
+   enable_dns_hostnames = true
+   enable_dns_support = true 
 }
--- aws/backand.tf
+++ aws/backand.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-class-andrii"
-    key    = "path/to/my/key"
+    key    = "path/to/my/key/terraform.tfvars"
     region = "us-east-1"
     #dynamodb_table = "terraform-class"
   }
