8bc6099bcd7064d3d8aa84648a5cec8828aa8fd0
Cloud User <centos@ip-172-31-17-131.eu-west-2.compute.internal>
2020-03-28 07:16:29 +0000

    update

--- aws/3-tier-app/data_source.tf
+++ aws/3-tier-app/data_source.tf
@@ -3,7 +3,7 @@
 data "terraform_remote_state" "dev" {
     backend = "s3"
     config = {
-        bucket = "terraform-class-bek"
+        bucket = "terraform-class-andrii"
         key = "tower/us-east-1/tools/virginia/tower.tfstate"
         region = "us-east-1"
     }
