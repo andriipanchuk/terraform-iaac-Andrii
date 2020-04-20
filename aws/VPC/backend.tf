f7c807b57f0536b02ab54a9e72e571c3d3b42ea7
Cloud User <centos@ip-172-31-27-1.eu-west-2.compute.internal>
2020-04-20 19:10:38 +0000

    1

--- aws/VPC/backend.tf
+++ aws/VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
-bucket = "terraform-class-andrii"
-key = "tower/us-east-1/tools/california/tower.tfstate"
-region = "us-east-1"
+bucket = "test-andrii-backet"
+key = "tower/us-west-1/tools/california/tower.tfstate"
+region = "us-west-1"
   }
 }
